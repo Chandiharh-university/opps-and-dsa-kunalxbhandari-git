fadef78b5a97951d9bb1630308484e878d8c90c6
kunalxbhandari-git <kunalxbhandari@gmail.com>
2024-12-24 10:56:39 +0530

    Create tree.cpp

---
+++ tree.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+// Node structure
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor
+    Node(int value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+// Insert node into binary tree
+Node* insertNode(Node* root, int value) {
+    if (root == nullptr) {
+        return new Node(value);
+    }
+
+    if (value < root->data) {
+        root->left = insertNode(root->left, value);
+    } else {
+        root->right = insertNode(root->right, value);
+    }
+
+    return root;
+}
+
+// In-order traversal (Left -> Root -> Right)
+void inOrderTraversal(Node* root) {
+    if (root != nullptr) {
+        inOrderTraversal(root->left);
+        cout << root->data << " ";
+        inOrderTraversal(root->right);
+    }
+}
+
+// Main function
+int main() {
+    Node* root = nullptr;
+    
+    // Insert nodes into the binary tree
+    root = insertNode(root, 50);
+    root = insertNode(root, 30);
+    root = insertNode(root, 20);
+    root = insertNode(root, 40);
+    root = insertNode(root, 70);
+    root = insertNode(root, 60);
+    root = insertNode(root, 80);
+
+    // Display in-order traversal
+    cout << "In-order Traversal: ";
+    inOrderTraversal(root);
+    cout << endl;
+
+    return 0;
+}
