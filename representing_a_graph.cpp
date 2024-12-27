8e9c6c2f91785b3cab13eff1fb60dbe08a70122c
kunalxbhandari-git <kunalxbhandari@gmail.com>
2024-12-27 09:49:03 +0530

    Create representing_a_graph.cpp

---
+++ representing_a_graph.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Graph {
+public:
+    int V;
+    vector<vector<int>> adjList;
+
+    Graph(int V) {
+        this->V = V;
+        adjList.resize(V);
+    }
+
+    void addEdge(int u, int v) {
+        adjList[u].push_back(v);
+        adjList[v].push_back(u); // Undirected graph
+    }
+
+    void display() {
+        for (int i = 0; i < V; i++) {
+            cout << i << ": ";
+            for (int v : adjList[i]) {
+                cout << v << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    Graph g(5);
+    g.addEdge(0, 1);
+    g.addEdge(0, 4);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(1, 4);
+    g.addEdge(2, 3);
+    g.addEdge(3, 4);
+
+    g.display();
+    return 0;
+}
